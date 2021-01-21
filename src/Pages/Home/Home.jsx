a190c473e987787ed12b5da71f34eb911bfc5301
imbhabani222 <imbhabani222@gmail.com>
2021-01-21 13:43:49 +0530

    Updated commit

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,35 @@
+import { useHistory } from 'react-router-dom';
+import './Home.css'
+const Home = () => {
+    const history = useHistory();
+    const handleSubmit = () => {
+        //alert("clicked");
+        history.push("/dashboard");
+    }
+    return (
+        <div>
+
+            <div className="row">
+                <div className="col">
+                    <h1> Learn to code by watching others </h1>
+                    <p> See how experienced developers solve problems in real-time Watching scripred tutorials is great.but understanding how developers think is invaluable</p>
+                </div>
+                <div className="col">
+                    <div className="header">
+                        <b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Try it free 7 days </b> than $20/mo.thereafter
+                    </div>
+                    <form onSubmit={handleSubmit}>
+                        <input type="text" placeholder="First Name" required />
+                        <input type="text" placeholder="Last Name" required />
+                        <input type="email" placeholder="Email Address" required />
+                        <input type="password" placeholder="Password" required />
+                        <button className="myButton">CLAIM YOUR FREE TRIAL</button>
+                        <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;By clicking the button,you are agreeing to our<b> Terms and Services</b></p>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    );
+}
+export default Home;
